da4054cac564e8b561791d84ec79d8bcaec8677d
YanochkaSp <sharmgala@mail.ru>
2026-01-27 19:49:59 +0300

    Add tree builder

--- task94/src/main.rs
+++ task94/src/main.rs
@@ -3,32 +3,32 @@ struct TreeNode {
     left: Option<Box<TreeNode>>,
     right: Option<Box<TreeNode>>,
 }
-fn main() {
-    let mut res = Vec::new();
-    fn dfs(node: Option<Box<TreeNode>>, res: &mut Vec<u8>) {
-        if let Some(n) = node {
-            dfs(n.left, res);
-            res.push(n.val);
-            dfs(n.right, res);
-        }
+fn main() {}
+
+fn dfs(node: Option<Box<TreeNode>>, res: &mut Vec<u8>) {
+    if let Some(n) = node {
+        dfs(n.left, res);
+        res.push(n.val);
+        dfs(n.right, res);
     }
-    let root = Some(Box::new(TreeNode {
-        val: 1,
-        left: None,
-        right: Some(Box::new(TreeNode {
-            val: 2,
-            left: Some(Box::new(TreeNode {
-                val: 3,
-                left: None,
-                right: None,
-            })),
-            right: None,
-        })),
-    }));
-    dfs(root, &mut res);
-    println!("{:?}", res);
 }
 
-fn build_tree() {
-    
+fn build_tree_recursive(vals: &[Option<u8>], index: usize) -> Option<Box<TreeNode>> {
+    if index >= vals.len() || vals[index].is_none() {
+        return None;
+    }
+
+    Some(Box::new(TreeNode {
+        val: vals[index].unwrap(),
+        left: build_tree_recursive(vals, 2 * index + 1),
+        right: build_tree_recursive(vals, 2 * index + 2),
+    }))
+}
+
+#[test]
+fn simple_test() {
+    let mut res = Vec::new();
+    let root = build_tree_recursive(&vec![Some(1), None, Some(2), None, None, Some(3), None], 0);
+    dfs(root, &mut res);
+    assert_eq!(res, [1, 3, 2]);
 }
